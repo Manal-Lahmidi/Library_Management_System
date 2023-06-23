f308d96605ae14c3d3f723eb24ae2c8dbe2a3f60
Manal-Lahmidi <manoula8987gmail.com>
2023-06-23 21:46:02 +0100

    stylesheet name updated + favicond added

--- EmpruntManagement.php
+++ EmpruntManagement.php
@@ -121,7 +121,7 @@ $livresDisponibles = mysqli_fetch_all($res, MYSQLI_ASSOC);
     <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
     <meta charset="UTF-8">
     <title>Gestion des emprunts</title>
-    <link rel="stylesheet" href="stylee.css">
+    <link rel="stylesheet" href="formStyle.css">
     <link rel="apple-touch-icon" sizes="180x180" href="imgs/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="imgs/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="imgs/favicon-16x16.png">
--- updateBook.php
+++ updateBook.php
@@ -14,7 +14,12 @@ require('config.php');
 	<head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
 		<title>book management</title>
-	    <link rel="stylesheet" href="style.css">
+	    <link rel="stylesheet" href="formStyle.css">
+        <link rel="apple-touch-icon" sizes="180x180" href="imgs/apple-touch-icon.png">
+		<link rel="icon" type="image/png" sizes="32x32" href="imgs/favicon-32x32.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="imgs/favicon-16x16.png">
+		<link rel="icon" type="image/x-icon" href="imgs/favicon.ico">
+		<link rel="manifest" href="site.webmanifest">
 	</head>
     <body>
         <form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="post" class="form">
